849cd705a709d8c842a36094ffe3f1342d3b418f
dude8748@gmail.com <35315317+amiyourtype@users.noreply.github.com>
2021-08-06 13:24:08 -0700

    first commit (everything so far)

---
+++ Cheese Fighter/Assets/Scripts/JumpRing.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpRing : MonoBehaviour
+{
+    private float timer = 27f;
+    private float width = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(exist());
+    }
+
+    IEnumerator exist()
+    {
+        while (timer > 0f)
+        {
+            transform.localScale = new Vector3(width, 1f, 1f);
+            width += 0.2f;
+            timer -= 1f;
+            yield return Utils.Frames(1);
+        }
+        Destroy(gameObject);
+    }
+}
---
+++ Cheese Fighter/Assets/Scripts/Trail.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trail : MonoBehaviour
+{
+    public SpriteRenderer sr;
+    private float duration;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        duration = 1f;
+        StartCoroutine(exist());
+    }
+
+    IEnumerator exist()
+    {
+        while (duration > 0f)
+        {
+            sr.color = new Color (1f, 1f, 1f, duration);
+            duration -= 0.01f;
+            yield return Utils.Frames(1);
+        }
+        Destroy(gameObject);
+    }
+}

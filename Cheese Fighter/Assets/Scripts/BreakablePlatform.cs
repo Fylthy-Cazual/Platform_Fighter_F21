095224afb0047b4bae9ac0f5cb964f087e650400
Girlcoding22 <31711391+Girlcoding22@users.noreply.github.com>
2021-10-24 14:38:33 -0700

    Breakable Platform created
    Created script that destroys the platform/object it is on.

---
+++ Cheese Fighter/Assets/Scripts/BreakablePlatform.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakablePlatform : MonoBehaviour
+{
+    [SerializeField]
+    private float timer; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        //set timer variable to 5 seconds(or whatever is the fps it is)
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer > 0){
+            //does nothing
+            timer -= 1;
+        }
+        else{
+            //breakapart
+            Destroy(gameObject);
+
+        }
+
+        //maybe make the if question for it here
+        //if out of range: remove platform, and if character still on platform be dropped straight down to the stage of where platform is hovering over
+        //else counter-1,
+        
+    }
+}

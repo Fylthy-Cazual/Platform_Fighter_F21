65daefcf3108274b80d5ea3892288b4cc5c49550
recko10 <adith.reddi@gmail.com>
2021-11-12 22:59:17 -0800

    added skeleton

---
+++ Cheese Fighter/Assets/Scripts/Rats/Mafia.cs
@@ -0,0 +1,190 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Mafia : Rat
+{
+    
+    // -------------------------------------------------------------------------- STATIC MEMBERS
+    #region STATIC MEMBERS
+    #endregion
+
+    // -------------------------------------------------------------------------- SERIALIZABLE INSPECTOR
+    #region SERIALIZABLE INSPECTOR
+    #endregion
+
+    // -------------------------------------------------------------------------- INSTANCE PROPERTIES
+    #region INSTANCE PROPERTIES
+    #endregion
+
+    // -------------------------------------------------------------------------- GETTERS AND SETTERS
+    #region GETTERS AND SETTERS
+    #endregion
+
+    // -------------------------------------------------------------------------- INITIALIZATION
+    #region INITIALIZATION
+    #endregion
+
+    // -------------------------------------------------------------------------- METHODS AND ROUTINES
+    #region METHODS AND ROUTINES
+    protected override IEnumerator jabG() //grounded normal attack
+    {
+
+        action = true;
+        animator.SetTrigger(Jab_Ground);
+        yield return Utils.Frames(25);
+        Projectile p = makeProjectile(0.3f * dir, 0f, 1.6f, 
+            5f, 15, 10, 
+            1.5f * dir, 0.5f, 5, 
+            2); //How can we increase the range to make the jab a "shooting" action?
+        p.setSpeedX(50f * dir); //Jerry will this increase the speed? We can't tell because the animation will play at a constant rate.
+        if (dir < 0) p.flip();
+        yield return Utils.Frames(10);
+        action = false;
+        animator.SetTrigger(Return);
+    }
+
+    protected override IEnumerator jabA() //aerial normal attack
+    {
+
+        //Slam straight down
+
+        // PARAMS
+        const int setupDuration = 28;
+        const int dashDuration = 3;
+        const float setupSpeed = 0.1f;
+        const float dashSpeed = 0.1f;
+
+        action = true;
+        animator.SetTrigger(Jab_Air);
+        for (int i = 0; i < setupDuration; i++)
+        {
+            Transform myTransform = transform;
+            Vector3 myPosition = myTransform.position;
+            myPosition = new Vector3(myPosition.x,
+                myPosition.y + setupSpeed,
+                myPosition.z);
+            myTransform.position = myPosition;
+            yield return Utils.Frames(1);
+        }
+        
+        while (!collidingY) {
+            makeHitbox(0, 0, 4f,
+                3f, dashDuration, 40,
+                5 * dir, 4, 30,
+                5);
+
+            for (int i = 0; i < dashDuration; i++)
+            {
+                Transform myTransform = transform;
+                Vector3 myPosition = myTransform.position;
+                myPosition = new Vector3(myPosition.x,
+                    myPosition.y - dashSpeed,
+                    myPosition.z);
+                myTransform.position = myPosition;
+                yield return Utils.Frames(1);
+            }
+        }
+        
+        animator.SetTrigger(Return);
+
+        yield return Utils.Frames(10);
+        action = false;
+    }
+
+    protected override IEnumerator specialG() //grounded special attack
+    {
+        //Two henchmen appear and shoot in the two cardinal directions
+        //The mafia rat is in the middle; can still be damaged
+        // PARAMS
+        const int setupDuration = 20;
+        const int dashDuration = 40;
+        const float setupSpeed = 0.04f;
+        const float dashSpeed = 0.05f;
+        
+        action = true;
+        animator.SetTrigger(Special_Ground); 
+        for (int i = 0; i < setupDuration; i++)
+        {
+            Transform myTransform = transform;
+            Vector3 myPosition = myTransform.position;
+            myPosition = new Vector3(myPosition.x + (setupSpeed * dir),
+                myPosition.y,
+                myPosition.z);
+            myTransform.position = myPosition;
+            yield return Utils.Frames(1);
+        }
+        makeHitbox(0, 0, 4f,
+            3f, dashDuration, 60,
+            2f * dir, 3.2f, 40,
+            5);
+        for (int i = 0; i < dashDuration; i++)
+        {
+            Transform myTransform = transform;
+            Vector3 myPosition = myTransform.position;
+            myPosition = new Vector3(myPosition.x + (dashSpeed * dir),
+                myPosition.y,
+                myPosition.z);
+            myTransform.position = myPosition;
+            yield return Utils.Frames(1);
+        }
+        yield return Utils.Frames(3);
+        action = false;
+        animator.SetTrigger(Return);
+    }
+
+    protected override IEnumerator dash()
+    {
+        //Dashing through rats; the more rats you dash through, the more damage you do
+        //If the rat gets hit while dashing, stop dashing and reset the damage
+
+        // PARAMS
+        const int setupDuration = 10;
+        const int dashDuration = 10;
+        const float setupSpeed = 0.04f;
+        const float dashSpeed = 0.08f;
+        
+        action = true;
+        animator.SetTrigger(Dash); 
+        for (int i = 0; i < setupDuration; i++)
+        {
+            Transform myTransform = transform;
+            Vector3 myPosition = myTransform.position;
+            myPosition = new Vector3(myPosition.x + (setupSpeed * dir),
+                myPosition.y,
+                myPosition.z);
+            myTransform.position = myPosition;
+            yield return Utils.Frames(1);
+        }
+        makeHitbox(0, 0, 4f,
+                    3f, dashDuration, 40,
+                    1f * dir, 1.6f, 20,
+                    5);
+        for (int i = 0; i < dashDuration; i++)
+        {
+            Transform myTransform = transform;
+            Vector3 myPosition = myTransform.position;
+            myPosition = new Vector3(myPosition.x + (dashSpeed * dir),
+                myPosition.y,
+                myPosition.z);
+            myTransform.position = myPosition;
+            yield return Utils.Frames(1);
+        }
+        yield return Utils.Frames(10);
+        action = false;
+        animator.SetTrigger(Return);
+    }
+    #endregion
+
+    // -------------------------------------------------------------------------- UNITY EVENT FUNCTIONS
+    #region UNITY EVENT FUNCTIONS
+    #endregion
+
+    // -------------------------------------------------------------------------- CASTING
+    #region CASTING
+    #endregion
+
+    // -------------------------------------------------------------------------- HELPER CLASSES
+    #region HELPER CLASSES
+    #endregion
+    
+}
